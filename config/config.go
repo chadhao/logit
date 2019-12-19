1ebf190deac1cf7c4d8bab515bc2c85c874093e2
chadhao <me@chadhao.com>
2019-12-19 17:28:44 +0800

    go mod update

--- config/config.go
+++ config/config.go
@@ -1,4 +1,4 @@
-package config
+package github.com/chadhao/logit/config
 
 var Config = make(map[string]string, 0)
 
--- modules/user/main.go
+++ modules/user/main.go
@@ -1,4 +1,4 @@
-package user
+package github.com/chadhao/logit/modules/user
 
 import (
 	"github.com/labstack/echo/v4"
